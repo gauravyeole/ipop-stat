0aacc9a006d70bd36d8d0567125d5373d7b830d6
gauravyeole <gauravyeole@gmail.com>
2018-02-15 14:05:07 -0500

    working ipop-stat0.15

--- ipopstat-0.15/usr/share/ipop-stat/run.py
+++ ipopstat-0.15/usr/share/ipop-stat/run.py
@@ -8,6 +8,7 @@ from ipop_stats.app import create as create_app
 import argparse
 import os, os.path
 
+
 parser = argparse.ArgumentParser(description="Standalone test server for "
                                              "ipop-stats")
 
@@ -31,3 +32,5 @@ app = create_app(**vars(args)) #run(host="0.0.0.0", port=8080)
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(8080)
 IOLoop.instance().start()
+
+
